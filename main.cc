7e4137a78130e6d800fb3b36f143044149c45e70
yyy <2176180685@qq.com>
2024-11-20 20:17:21 +0800

    写了main.ccc的框架

---
+++ main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+void sort(int arr[], int size) {
+
+}
+int main() {
+	int arr[] = { 23,89,45,67,43,78 };
+	int size = sizeof(arr) / sizeof(int);
+	sort(arr, int size);
+	return 0;
+}
