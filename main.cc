95447d555ea89bb97c40ac22bf993ce788794395
yyy <2176180685@qq.com>
2024-11-22 12:33:31 +0800

    实现了sort函数

--- main.cc
+++ main.cc
@@ -1,6 +1,14 @@
 #include <iostream>
 void sort(int arr[], int size) {
-
+    for(int i=0;i<size-1;++i){
+        for(int j=0;j<size-1-i;++j){
+            if(arr[j]>arr[j+1]){
+                int tmp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=tmp;
+            }
+        }
+    }
 }
 int main() {
 	int arr[] = { 23,89,45,67,43,78 };
